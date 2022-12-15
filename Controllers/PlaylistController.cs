8374e3a87e9f3c90fe3dfa296c9b9131b0ca3e08
shaaban01 <91268135+shaaban01@users.noreply.github.com>
2022-12-15 23:32:08 +0200

    playlist

--- Controllers/PlaylistController.cs
+++ Controllers/PlaylistController.cs
@@ -11,31 +11,18 @@ namespace Songify.Controllers
     public class PlaylistController : ControllerBase
     {
         private readonly DataContext _context;
-        public static Playlist playlist = new Playlist();
-        public static List<Playlist> playlists = new List<Playlist>();
-        private readonly IConfiguration _configuration;
-        private readonly IPlaylistService _playlistService;
-
-        public PlaylistController(IConfiguration configuration, IPlaylistService playlistService, DataContext context)
+    
+        [HttpGet]
+        public ActionResult<string> GetAll(String user)
         {
-            _context = context;
-            _configuration = configuration;
-            _playlistService = playlistService;
-        }
-
-        [HttpGet, Authorize]
-        public ActionResult<string> GetMe()
-        {
-            var userName = _playlistService.GetMyPlaylist();
+            IEnumerable<Playlist> Playlists = ;
             return Ok(userName);
         }
 
-        [HttpPost("register")]
         public async Task<ActionResult<User>> Register(Playlist request)
         {
             playlist.Id = request.Id;
-            playlist.playlist.Add(request.AddSong());
-
+            playlist.playlist.Add(request.AddSong())
             playlists.Add(playlist);
             _context.playlist.Add(playlist);
             await _context.SaveChangesAsync();
